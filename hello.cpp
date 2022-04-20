0a2aff45afb08db4555bec6e20e8aa64c8926f72
Aaron Jones <jon17049@byui.edu>
2022-04-20 13:45:54 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // A header file library that works with input and output objects
+using namespace std; // Gives the user the ability to use names for objects and variables from the standard library
 
-int main() {
-  cout << "Hello World!";
-    return 0;
-}
+int main() { // This is a function and any code inside its curly brackets {} will be executed.
+  cout << "Hello World!"; // An object used together with the insertion operator (<<) to output/print text.
+    return 0; // ends the main function
+} // This ends the main function
